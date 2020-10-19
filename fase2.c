148a1a8fa6663483067c326bcb46ab3225c4a3fc
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2020-10-19 15:32:46 -0300

    atualizacao fase2
    adicionado sistema de derrota, retry e vitoria, alem de fundo de mapa.

--- fase2.c
+++ fase2.c
@@ -27,6 +27,7 @@ typedef struct Enviroment
 
 Envment base;
 Envment tiles[8];
+
 Texture2D tilesTxt;
 Rectangle tilesRect;
 
@@ -35,33 +36,77 @@ Rectangle portalrect;
 
 Texture2D backgroundFase2;
 Rectangle backgroundFase2rect;
-    
-Camera2D cameraJumper = {0};
-int frames=0;
-    int playerTxtFrames = 0;
-    int framecontPlayer = 0;
-    int framespeedPlayer = 9;
 
-    int portalFrames=0;
-    int framecontportal=0;
-    int portalmode=0;
-
+Rectangle cristal;
+Rectangle cristalrect;
+Texture2D cristaltxt;
     
-    int i = 0;
-    int rando = 0;
-    int randwidth = 0;
-    int singleTile = 50;
-    int segundos = 0;
-    bool repetido = false;
-    bool lost = false;
-    bool portalativo = true;
-Player player = {0};
+Camera2D cameraJumper;
+    float paralaxFase2;
+    int frames;
+    int playerTxtFrames;
+    int framecontPlayer ;
+    int framespeedPlayer ;
+
+    int portalFrames;
+    int framecontportal;
+    int portalmode;
+    
+    int framecontCristal;
+    
+    int i;
+    int rando;
+    int randwidth;
+    int singleTile;
+    int segundos;
+
+    bool repetido;
+    bool lost;
+    bool victoryFase2;
+    bool gotCristal;
+    bool portalativo;
+    bool endgame;
+    bool replayfase2;
+Player player;
 
 
 void initfase2(void)
 {
+
 SetConfigFlags(FLAG_MSAA_4X_HINT);
 InitPhysics();
+    repetido = false;
+     lost = false;
+     victoryFase2 = false;
+     gotCristal = false;
+     portalativo = true;
+     endgame =false;
+     replayfase2 = false;
+
+     paralaxFase2=0.5;
+     frames=0;
+     playerTxtFrames = 0;
+     framecontPlayer = 0;
+     framespeedPlayer = 9;
+
+     portalFrames=0;
+     framecontportal=0;
+     portalmode=0;
+
+    framecontCristal = 0;
+    
+     i = 0;
+     rando = 0;
+     randwidth = 0;
+     singleTile = 50;
+     segundos = 0;
+
+     cristaltxt = LoadTexture("assets/cristalVermelho.png");
+     cristaltxt.width = 200;
+     cristaltxt.height = 50;
+     cristal = (Rectangle){0,0,cristaltxt.width/4,cristaltxt.height};
+     cristalrect = (Rectangle){0,0,cristaltxt.width/4,cristaltxt.height};
+
 //TEXTURA DO PLAYER
 
 player.txt = LoadTexture("assets/Fase2/half.png");
@@ -88,13 +133,16 @@ portalFase2 = LoadTexture("assets/Portal.png");
 portalFase2.height *=3;
 portalFase2.width *=4;
 portalrect = (Rectangle){0,0,portalFase2.width/8,portalFase2.height/3};
-//portalrect.y = portalFase2.height/3;
 
+backgroundFase2 = LoadTexture("assets/Fase2/window.png");
+backgroundFase2.width *=10 ;
+backgroundFase2.height *=10;
+backgroundFase2rect = (Rectangle) {0,0,backgroundFase2.width,backgroundFase2.height};
 
 PhysicsBody base = CreatePhysicsBodyRectangle((Vector2){ 0, 50 }, 450, 100, 10);
 base->enabled = false;
 
-    cameraJumper.target = (Vector2) {0,0};
+    cameraJumper.target = (Vector2) {0,-100};
     cameraJumper.zoom = 0.7f;
     cameraJumper.offset = (Vector2) {larguraDaTela/2,AlturaDaTela/2};
     
@@ -104,7 +152,11 @@ base->enabled = false;
  void Refresh(){
     //ATUALIZANDO FRAMES PARA SPRITES
     frames++;
-
+    if(victoryFase2){
+        if(frames%15==0)framecontCristal++;
+        if(framecontCristal>=4)framecontCristal=0;
+        cristal.x = framecontCristal*cristaltxt.width/4;
+    }
     if(frames%60==0)segundos++;
 
      if (segundos>4) portalativo = false;
@@ -118,9 +170,6 @@ base->enabled = false;
                 framecontportal=0;
             }
             portalrect.x = framecontportal*portalFase2.width/8;
-            /*if(portalmode==1){
-                portalrect.y = 0;
-            }*/
             if(portalmode>=2){
                 portalrect.y =  2*portalFase2.height/3;
             }   
@@ -138,7 +187,7 @@ base->enabled = false;
         }
         //SPAWN DE PLATADORMAS
        
-        if(!portalativo){
+        if(!portalativo&&!lost&&!gotCristal){
         
         if(frames%120==0){
        // frames = 0;
@@ -182,23 +231,22 @@ base->enabled = false;
         player.body->velocity = veloc;
 
         }
-        
+    
         
         // MOVIMENTACAO
 
-        if (IsKeyDown(KEY_RIGHT)){ player.body->velocity.x = VELOCITY;player.direita = true;}
-        else if (IsKeyDown(KEY_LEFT)){ player.body->velocity.x = -VELOCITY;player.direita = false;}
+        if (IsKeyDown(KEY_RIGHT)){ player.body->velocity.x = VELOCITY*1.1;player.direita = true;}
+        else if (IsKeyDown(KEY_LEFT)){ player.body->velocity.x = -VELOCITY*1.1;player.direita = false;}
         else player.body->velocity.x = 0;
         if (IsKeyDown(KEY_UP) && player.body->isGrounded) player.body->velocity.y = -VELOCITY*5;
 
-        
-        
-
         //ATUALIZACAO DA CAMERA
         cameraJumper.target.y -= 1.2f;
+        if(cameraJumper.target.y<(-200)) paralaxFase2 -=0.8;
+        }
         if(player.body->position.y>=cameraJumper.target.y+AlturaDaTela*1.5) lost = true;
         if(IsKeyPressed(KEY_ZERO)) lost = true;
-        }
+        
         //ATUALIZACAO DAS SPRITES
 
         if(player.body->velocity.x==0 && player.direita){  
@@ -209,7 +257,19 @@ base->enabled = false;
         if(player.direita) player.rect.y = 0;
         else player.rect.y = player.txt.height/4;
         }
-    
+        if(segundos>=20){
+        victoryFase2 = true;
+        cristalrect.x = 0;
+        cristalrect.y = cameraJumper.target.y-400;
+        }
+        if(gotCristal&&IsKeyPressed(KEY_ZERO)) endgame=true;
+        if(lost&&IsKeyPressed(KEY_F)) endgame=true;
+        if(lost&&IsKeyPressed(KEY_R)){
+            lost = false;
+            endgame=true;
+            replayfase2 = true;
+        }
+        if(CheckCollisionPointRec(player.body->position,cristalrect)) gotCristal = true;
  }
     
 
@@ -217,7 +277,7 @@ base->enabled = false;
         BeginDrawing();
         BeginMode2D(cameraJumper);
             ClearBackground(RAYWHITE);
-
+            DrawTexture(backgroundFase2,-backgroundFase2.width/2,-backgroundFase2.height+AlturaDaTela/1.5+paralaxFase2,WHITE);
            //DESENHO DO MAPA
             for(int x=0;x<8;x++){
                 int rectsize = tiles[x].rect.width/50;
@@ -226,34 +286,58 @@ base->enabled = false;
             
                 }
             }
-
+            if(victoryFase2){
+                DrawTextureRec(cristaltxt,cristal,(Vector2){0,cameraJumper.target.y-400},WHITE);
+            }
+            
+            
             //DESENHO DO PLAYER
 
-            //if(!portalativo)
+            if(!lost&&!gotCristal){
             DrawTextureRec(player.txt,player.rect,(Vector2){player.body->position.x-25,player.body->position.y-25},WHITE);
-            DrawText(TextFormat("%d", segundos),cameraJumper.target.x+700,cameraJumper.target.y+400,50,RED);
-            if(portalativo)DrawTextureRec(portalFase2,portalrect,(Vector2){player.body->position.x-120,player.body->position.y-100},WHITE);
-            EndMode2D();
             
+            if(portalativo)DrawTextureRec(portalFase2,portalrect,(Vector2){player.body->position.x-120,player.body->position.y-100},WHITE);
+            }
+           
 
+            
+            EndMode2D();
+            DrawText(TextFormat("%d", segundos),1100,600,50,RED);
+            if(victoryFase2&&gotCristal){
+                DrawRectangleGradientH(0,0,1280,720,DARKGRAY,WHITE);
+                DrawText("YOU WON!!!",300,360,100,PURPLE);
+                DrawText("pressione 0 para voltar",100,700,20,RED);
+            }
+            else if(lost){
+                DrawRectangleGradientH(0,0,1280,720,DARKGRAY,BLACK);
+                DrawText("YOU LOST!!!",300,360,100,RED);
+                DrawText("pressione R para tentar de novo,ou f para desistir",100,700,20,WHITE);
+            }
             
         EndDrawing();
         
     }
 void fechar(){
 UnloadTexture(player.txt);
-UnloadTexture(tiles->txt);  
+UnloadTexture(tilesTxt);
+for(int z=0;z<8;z++){
+    DestroyPhysicsBody(tiles[z].body);
+    tiles[z].rect = (Rectangle){0,0,0,0};
+            
+}
 ClosePhysics();       
      
 }
    
 int Fase2(){
     initfase2();
-while(!lost){
+while(!endgame){
     RunPhysicsStep();
     Refresh();
     Desenhar();
 }
     fechar();
-return 0;
+    if(gotCristal) return 2;
+    else if(replayfase2) return 1;
+    else return 3;
 }
